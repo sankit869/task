781ea810f262885ed6055093d9508eb6c7d83ed7
ankit20.sharma <ankit20.sharma@paytmmoney.com>
2020-04-28 23:48:24 +0530

    modify

--- src/test/java/fisdomSpecs/BaseTest.java
+++ src/test/java/fisdomSpecs/BaseTest.java
@@ -1,13 +1,10 @@
 package fisdomSpecs;
 
-import apiservice.FisdomService;
 import io.restassured.RestAssured;
 import org.testng.annotations.BeforeClass;
-import org.testng.annotations.BeforeTest;
 import org.testng.asserts.SoftAssert;
 
 public class BaseTest {
-    public FisdomService fisdomService = new FisdomService();
     public SoftAssert softAssert = new SoftAssert();
     @BeforeClass
     public void setBaseURI(){
